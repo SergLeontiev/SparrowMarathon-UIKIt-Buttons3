8f46043d5915ef24f5be8351419243e8e8a571d3
Sergey Leontiev <translatertrans@gmail.com>
2024-11-04 18:53:26 +0100

    Init project

---
+++ SparrowMarathon-UIKIt-Buttons3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SparrowMarathon-UIKIt-Buttons3
+//
+//  Created by Sergey Leontiev on 4.11.24..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
